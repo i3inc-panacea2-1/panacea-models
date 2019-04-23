3631b37451803dc3b85132390d10f6a5f587ff97
Portno <j.remoundos@dotbydot.gr>
2019-04-23 12:13:29 +0300

    added company details

--- src/Panacea.Models/Properties/AssemblyInfo.cs
+++ src/Panacea.Models/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Panacea.Models")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Basic server-side models for Panacea.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("i3inc")]
 [assembly: AssemblyProduct("Panacea.Models")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
