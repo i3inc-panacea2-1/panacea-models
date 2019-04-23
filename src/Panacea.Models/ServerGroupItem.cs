06bda9d03d0617da8f4da3659dba3948c11c2ae2
Portno <j.remoundos@dotbydot.gr>
2019-04-23 12:11:29 +0300

    first commit

---
+++ src/Panacea.Models/ServerGroupItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Panacea.Models
+{
+    [DataContract]
+    public class ServerGroupItem : ServerItem
+    {
+        [DataMember(Name = "items")]
+        public ObservableCollection<ServerItem> Items { get; set; }
+    }
+}
---
+++ src/Panacea.Models/ServerItem.cs
@@ -0,0 +1,31 @@
+﻿using Panacea.Multilinguality;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Panacea.Models
+{
+    [DataContract]
+    public class ServerItem : Translatable
+    {
+        [DataMember(Name = "_id")]
+        public string Id { get; set; }
+
+        [IsTranslatable]
+        [DataMember(Name = "name")]
+        public string Name
+        {
+            get => GetTranslation();
+            set => SetTranslation(value);
+        }
+
+        [DataMember(Name = "img_thumbnail")]
+        public Thumbnail ImgThumbnail { get; set; }
+
+        [DataMember(Name = "categories")]
+        public List<string> Categories { get; set; }
+    }
+}
